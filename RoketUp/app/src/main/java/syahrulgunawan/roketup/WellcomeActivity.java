5a90a2e7e4109e742e2cd389e2a3e39fe1c24ab2
1syahrulgunawan1 <1syahrulgunawan1@gmail.com>
2018-04-25 11:43:15 +0700

    :100:

---
+++ RoketUp/app/src/main/java/syahrulgunawan/roketup/WellcomeActivity.java
@@ -0,0 +1,40 @@
+package syahrulgunawan.roketup;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class WellcomeActivity extends AppCompatActivity {
+    LinearLayout l1,l2;
+    Animation uptodown,downtoup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wellcome);
+        l1 = (LinearLayout) findViewById(R.id.l1);
+        l2 = (LinearLayout) findViewById(R.id.l2);
+        uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
+        downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
+        l1.setAnimation(uptodown);
+        l2.setAnimation(downtoup);
+
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
